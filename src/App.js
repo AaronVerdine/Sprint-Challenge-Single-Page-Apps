e91ae18f5213281ec63da9d8f36acb260b8ce834
Your name <a.m.verdine@gmail.com>
2019-11-10 20:29:57 -0500

    Build out LocationList component/send Axios get request to location API/ log location API response to the console

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import TabNav from "./components/TabNav.js";
-import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage.js";
 import CharacterList from "./components/CharacterList";
-// import LocationsList from "./components/LocationsList"
+import LocationsList from "./components/LocationsList";
 
 export default function App() {
   return (
-    <main>
-      {/* <Header /> */}
-      {/* <WelcomePage /> */}
+    <div>
       <TabNav />
       {/* <CharacterList /> */}
       {/* <LocationsList/> */}
-    </main>
+
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
+      <Route path="/locations" component={LocationsList} />
+      {/* <Route path="/episodes" component={EpisodeList} /> */}
+    </div>
   );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-// export default function LocationsList(props) {
+export default function LocationsList() {
+  const [locations, setLocations] = useState([]);
 
-//     const locationObj = {
+  useEffect(id => {
+    axios
+      .get("https://rickandmortyapi.com/api/location/")
+      .then(res => {
+        console.log(res.data.results);
+        setLocations(res.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
-//     }
-//     const [locations, setLocations] = useState([])
-
-//     return (
-
-//     );
-// }
+  return <div></div>;
+}
